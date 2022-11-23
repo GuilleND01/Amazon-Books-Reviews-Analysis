4d00cf9e9f072d550c4b47f772971f12e78e30f8
Guille <gnovil01@ucm.es>
2022-11-23 23:37:23 +0100

    Update ratingsEvolution.py
    Cambiado para que pueda filtrar por el array

--- scripts/ratingsEvolution.py
+++ scripts/ratingsEvolution.py
@@ -3,27 +3,37 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 import pyspark.sql.functions as func
 import matplotlib.pyplot as plt
-
+import json
+import pyspark.sql.types as ty
 import sys
 
 conf = SparkConf().setMaster('local[*]').setAppName('mostRatings')
 sc = SparkContext(conf = conf)
 spark = SparkSession(sc)
 
+def parse_array_from_string(x):
+    res = json.loads(x)
+    return res
+
+retrieve_array = func.udf(parse_array_from_string, ty.ArrayType(ty.StringType()))
+
 cat = sys.argv[1]
 
+if ' and ' in cat:
+    cat = cat.replace(' and ', ' & ')
+
 # Lectura del archivo reviews
-df1Read = spark.read.json(".\DATASET MAS GRANDE\DATASET MAS GRANDE\meta_Books.json")
-df2Read = spark.read.json(".\DATASET MAS GRANDE\DATASET MAS GRANDE\Books_5.json")
+df1Read = spark.read.json('.\DATASET MAS GRANDE\DATASET MAS GRANDE\meta_Books.json')
+df2Read = spark.read.json('.\DATASET MAS GRANDE\DATASET MAS GRANDE\Books_5.json')
 
 df2Read = df2Read.join(df1Read, df1Read.asin == df2Read.asin, 'inner')
-df2Read.select('category', 'overall', 'reviewTime')
+df2Read = df2Read.select('reviewTime', 'category', 'overall')
 
-df2Read = df2Read.withColumn('category', df2Read['category'].cast('string'))
-df2Read = df2Read.filter(df2Read['category'].contains(cat))
-df2Read = df2Read.select('reviewTime', 'title', 'overall')
+df2Read = df2Read.filter(func.array_contains(df2Read['category'], cat))
+
 df2Read = df2Read.withColumn('reviewTime', df2Read['reviewTime'].cast('string'))
 
+df2Read.select('overall', 'reviewTime')
 df2Avg = df2Read.groupBy(df2Read['reviewTime'][-4:len(str(df2Read['reviewTime']))]).avg('overall')
 df2Avg = df2Avg.toDF(*('Year', 'Overall Score'))
 df2Avg = df2Avg.withColumn('Overall Score', func.round(df2Avg['Overall Score'], 2))
@@ -32,15 +42,15 @@ df2Avg = df2Avg.sort('Year')
 fig, ax = plt.subplots()
 
 # Paso a array los títulos y sus reviews
-titles = df2Avg.select(col('Year')).rdd.flatMap(lambda x: x).collect()
+years = df2Avg.select(col('Year')).rdd.flatMap(lambda x: x).collect()
 rating = df2Avg.select(col('Overall Score')).rdd.flatMap(lambda x: x).collect()
 
-# Pinto las barras y añado arriba el count exacto
-barGraphic = ax.scatter(titles, rating)
-plt.plot(titles, rating)
+# Se pintan y unen los puntos
+barGraphic = ax.scatter(years, rating)
+plt.plot(years, rating)
 
-ax.set_ylabel('EVOLUTION OF REVIEWS')
-ax.set_title(cat)
+ax.set_ylabel('Overall Score')
+ax.set_title('Evolution of ' + cat)
 
 plt.xticks(rotation=90)
-plt.savefig('evolution.png')
+plt.savefig(cat + '.png')
