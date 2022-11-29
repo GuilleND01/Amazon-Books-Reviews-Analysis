b5b25d72f1b5b7e4cb2bfef38ff78d27adb4a39f
jortega9 <91116613+jortega9@users.noreply.github.com>
2022-11-29 16:33:40 +0100

    Add files via upload

---
+++ scripts/mejoresLibrosCat.py
@@ -0,0 +1,43 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import pyspark.sql.functions as func
+from pyspark.sql.types import *
+
+
+conf = SparkConf().setMaster('local[*]').setAppName('mostRatings')
+sc = SparkContext(conf = conf)
+spark = SparkSession(sc)
+
+input_file1 = "./dataset/Books_5.json" #valoraciones
+input_file2 = "./dataset/meta_Books.json" #libros
+
+
+#numero de argumentos que se le pasan al programa
+num_args = len(sys.argv) # para lista de categorias bucle con esto para coger todas y al comprobar en el filter con todas ellas.
+cat = []
+for y in range(1,num_args):
+    cat.append(sys.argv[y])
+
+#cat = ["Books", "Children's Books", "Arts Music & Photography"]
+
+#leemos los ficheros
+
+dfVal = spark.read.json(input_file1)
+dfLib = spark.read.json(input_file2)
+
+dfRead = dfVal.join(dfLib, dfLib.asin == dfVal.asin, 'inner') #unimos los dos dataframes por el asin
+dfRead = dfRead.select('title', 'category', 'overall', "reviewerID") #seleccionamos las columnas que nos interesan
+
+#filtramos por las categorias que nos interesan
+dfFilter = dfRead.filter(func.array_contains(dfRead['category'], cat[0])) 
+for x in range(1,len(cat)):
+    dfFilter = dfFilter.filter(func.array_contains(dfRead['category'], cat[x]))
+
+
+avg1 = dfFilter.groupBy('title').agg({'overall': 'avg', 'reviewerID':'count'}).orderBy(col("count(reviewerID)").desc()) #calculamos la media de cada libro y cuantas valoraciones tiene
+avg1 = avg1.withColumnRenamed("avg(overall)", "rating") #renombramos la columna avg(overall) a avg
+avg1 = avg1.withColumnRenamed("count(reviewerID)", "vals") #renombramos la columna count(reviewerID) a count
+
+df_final = avg1.orderBy(col("rating").desc(), col("vals").desc()) #ordenamos por rating y por vals
+df_final.show(truncate = 1000) #mostramos el resultado
