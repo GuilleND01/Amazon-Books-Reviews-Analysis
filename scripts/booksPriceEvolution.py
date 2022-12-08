cc7250c32d56aad193040297d0596c4d589ccbad
jjencina <91247078+jjencina@users.noreply.github.com>
2022-12-08 20:51:51 +0100

    Update booksPriceEvolution.py

--- scripts/booksPriceEvolution.py
+++ scripts/booksPriceEvolution.py
@@ -13,11 +13,14 @@ sc = SparkContext(conf = conf)
 spark = SparkSession(sc)
 
 '''
-	Precio de los libros de una categoria a lo largo de un tiempo 
+	Libros de autores buscados segun el rango de precio
+		1er argumento Rango Bajo PRECIO
+		2do argumento Rango Alto PRECIO
+		3er en adelante argumentos NOMBRE AUTOR
 '''
 
 ##IMPORTANTE: 
-#Ejemplo de entrada:	spark-submit .\booksPriceEvolution.py 10-20-2018 10-20-2019 arts-and-crafts
+#Ejemplo de entrada:	spark-submit .\booksPerAuthorAndPrice.py 10 40 John Ruskin
 
 #numero de argumentos que se le pasan al programa
 num_args = len(sys.argv) # para lista de categorias bucle con esto para coger todas y al comprobar en el filter con todas ellas.
@@ -26,43 +29,32 @@ for y in range(1,num_args):
     cat.append(sys.argv[y])
 
 #author = "['Fiona Cownie']"
-#Fecha en formato dia-mes-año
-fecha1 = cat[0]
-fecha2 = cat[1]
+precio1 = cat[0]
+precio2 = cat[1]
 
-categoria = cat[2]
+precio1 = int(precio1)
+precio2 = int(precio2)
+
+
+#EL nombre del autor esta entre [''] en el dataframe asique se lo añado a la busqueda del usuario
+author = cat[2]
 for x in range(3,len(cat)):
-	categoria = categoria + " " + cat[x]
+	author = author + " " + cat[x]
 
 #Leo los datos de los libros
 input_file = "../dataset/meta_Books.json" #libros
 
 df = spark.read.json(input_file)
 
-#Selecciono las columnas de fecha, title y precio
-#Filtro los libros por categoria
-#Agrupo los libros por año y calxulo el precio medio de estos
-#Ordeno los resultados por año
+#Selecciono las columnas donde el autor es el que busco
+df = df.select(col('brand').alias("Autor"), col('title').alias("Titulo"), func.translate(func.col("price"), "$", "").alias("Precio"))\
+		.where((df['brand'] == author) | df['brand'].contains(author))
 
-#Selecciono las columnas de fecha, title y precio
-df = df.select('title'.alias("Título"), func.translate(func.col("price"), "$", "").alias("Precio")
-                , func.col("categories").alias("Categoría")
-                , func.col("date").alias("Fecha"))
+#Filtro los resultados segú el rango de precios seleccionado
+df = df.where(df["Precio"].between(precio1, precio2))
 
-df = df.filter(df["Categoría"].contains(categoria))
-
-#Sepero la fecha en dia, mes y año
-df = df.withColumn("Fecha", func.split(func.col("Fecha"), "-"))
-
-#Selecciono el año
-df = df.withColumn("Fecha", func.col("Fecha")[2])
-
-#Agrupo los libros por año y calculo el precio medio de estos
-df = df.groupBy("Fecha").agg(avg("Precio").alias("Precio medio"))
-
-#Ordeno los resultados por año
-df = df.orderBy("Fecha", ascending = True)
+df = df.groupby(col("Autor"),col("Titulo")).agg(avg("Precio").alias("PreciO")).orderBy("PreciO", ascending=False)
 
 df.show()
 
-df.coalesce(1).write.options(header = 'True', delimiter = ',').mode("overwrite").csv("../results/booksPriceEvolution.csv")
+df.coalesce(1).write.options(header = 'True', delimiter = ',').mode("overwrite").csv("../results/booksPerAuthorAndPrice.csv")
